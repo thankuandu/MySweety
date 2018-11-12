b42fddedd755bdd7745a6f567fcfdfb08db0f221
zhangzhuojia <zhangzhuojia2167@dingtalk.com>
2018-11-12 11:03:39 +0800

    first commit

---
+++ app/src/main/java/cn/zj/mysweety/MainActivity.java
@@ -0,0 +1,33 @@
+package cn.zj.mysweety;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+        pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+        pDialog.setTitleText("Loading");
+//        pDialog.setCancelable(false);
+        pDialog.show();
+    }
+}
